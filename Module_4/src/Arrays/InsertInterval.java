f9b9fb1d0e0c4d9f9ab427b334def68ae178ed90
deepmala-git <deepmala29496@in.ey.com>
2025-03-23 21:42:02 +0530

    adding comments

--- Module_4/src/Arrays/InsertInterval.java
+++ Module_4/src/Arrays/InsertInterval.java
@@ -12,19 +12,19 @@ public class InsertInterval {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
         int n = A.size();
         int count = 0;
-        while(count < n && (B.get(0) > A.get(count).get(1))){
+        while(count < n && (B.get(0) > A.get(count).get(1))){ //add interval which is not overlapping with given interval
             ans.add(A.get(count));
             count++;
         }
 
-        while(count < n && B.get(1) >= A.get(count).get(0)){
+        while(count < n && B.get(1) >= A.get(count).get(0)){ //merge overlapping interval
             B.set(0, Math.min(B.get(0), A.get(count).get(0)));
             B.set(1, Math.max(B.get(1), A.get(count).get(1)));
             count++;
         }
         ans.add(B);
 
-        while(count < n){
+        while(count < n){ //add rest of the interval
             ans.add(A.get(count));
             count++;
         }
